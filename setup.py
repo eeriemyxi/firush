f15236656aee1b232bf61faf377afec91dad370c
myxi <myxi@envs.net>
2025-03-14 19:31:31 +0530

    fix: update setup.py with latest info

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, Extension
+from setuptools import Extension, setup
 
 firush_api = Extension(
     "firush.api", libraries=["curl"], language="c", sources=["src/py/api/api.c"]
@@ -7,15 +7,14 @@ firush_api = Extension(
 setup(
     name="firush",
     version="1.0",
-    description="",
+    description="Find random nHentai manga/doujinshi/comic based on specified search query.",
     author="myxi",
     author_email="myxi@envs.net",
     packages=["firush"],
     package_dir={"firush": "src/py"},
-    entry_points = {"console_scripts": ["firush=firush:main"]},
+    entry_points={"console_scripts": ["firush=firush:main"]},
     install_requires=["nicegui"],
-    # package_data={"firush": ["api.pyi", "py.typed"]},
-    # url = 'https://docs.python.org/extending/building',
-    # long_description = open('README.md').read(),
+    url="https://github.com/eeriemyxi/firush/",
+    long_description=open("README.md").read(),
     ext_modules=[firush_api],
 )
